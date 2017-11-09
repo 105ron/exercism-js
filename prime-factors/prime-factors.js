38c721b53fb6ca74137fe3efa0625213297fb0a1
Rhys <rbrooker105@gmail.com>
2017-11-09 19:48:51 +1100

    refactor prime-factors exercise

--- prime-factors/prime-factors.js
+++ prime-factors/prime-factors.js
@@ -1,38 +1,17 @@
 const primeFactors  = {
-  isPrime (number) {
-    const q = Math.floor(Math.sqrt(number));
-    for (let i = 2; i <= q; i += 1) {
-      if (number % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  },
   for (number) { 
     let primesArray = [];
-    let divisor = number;
-    let noRemainder
-    let n;
-    if (number === 1) return [];
-    const factorial = primes => {
-      return primes.reduce(((sum, value) => sum * value),1)
-      };
-    while (factorial(primesArray) !== number) {
-      remainder = true;
-      n = 2;
-      while (remainder) {
-        if (this.isPrime(n) && divisor % n === 0) {
-          remainder = false;
-          primesArray.push(n)
-          divisor = divisor / n;
-        } else {
-          n += 1
-        }
+    let divisor = 2;
+    while (number > 1) {
+      if (number % divisor === 0) {
+        primesArray.push(divisor);
+        number /= divisor;
+        divisor = 2;
+      } else {
+        divisor += 1
       }
     }
     return primesArray;
   }
-
-
 }
 module.exports = primeFactors;
